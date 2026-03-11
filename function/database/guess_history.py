5a9f40dd30c3fa3ed0a7dbe7ffbef387fa2634e9
Moritz Pahlke <moritz.pahlke@uni-konstanz.de>
2026-03-11 13:48:42 +0100

    cleanup of word.py

--- function/database/guess_history.py
+++ function/database/guess_history.py
@@ -4,13 +4,13 @@ from database.models import GuessHistory
 
 
 def add_new_user_guess(session: Session, user_id: int, word_history_id: int, guess: str) -> None:
-    """Adds an user guess to the database"""
+    """Adds an user guess to the database."""
 
     session.add(GuessHistory(user_id=user_id, word_history_id=word_history_id, guess=guess))
 
 
 def get_user_guess_history(session: Session, user_id: int, word_history_id: int) -> list[GuessHistory]:
-    """Returns the user guess history for a given word"""
+    """Returns the user guess history for a given word."""
 
     return (
         session.query(GuessHistory)
--- function/database/word.py
+++ function/database/word.py
@@ -1,45 +1,52 @@
 import random
 from datetime import date
 
-from database.database import open_session
+from sqlalchemy.orm import Session
+
 from database.models import Language, Word, WordHistory
 
 
-def generate_word_today(language: Language) -> str:
-    with open_session() as session:
-        potential_words = session.query(Word).filter(Word.language == language, Word.potential_answer).all()
-        random_word = random.choice(potential_words)
-        new_word_history = WordHistory(word_id=random_word.id, date=date.today())
-        session.add(new_word_history)
+def generate_word_today(session: Session, language: Language) -> str:
+    """Function to generate a word in a given language for today."""
+
+    potential_words = session.query(Word).filter(Word.language == language, Word.potential_answer).all()
+    random_word = random.choice(potential_words)  # noqa: S311
+    new_word_history = WordHistory(word_id=random_word.id, date=date.today())
+    session.add(new_word_history)
     return random_word.word
 
 
-def get_word_today(language: Language) -> Word:
-    with open_session() as session:
-        word_entry = (
-            session.query(WordHistory)
-            .join(Word)
-            .filter(Word.language == language, WordHistory.date == date.today())
-            .first()
-        )
-        if word_entry is None:
-            raise ValueError
-        return word_entry.word
+def get_word_today(session: Session, language: Language) -> Word:
+    """Returns the word for today in the given language."""
+
+    word_entry = (
+        session.query(WordHistory)
+        .join(Word)
+        .filter(Word.language == language, WordHistory.date == date.today())
+        .first()
+    )
+    if word_entry is None:
+        raise ValueError
+    return word_entry.word
 
 
-def get_all_words(language: Language) -> list[str]:
-    with open_session() as session:
-        return [word.word for word in session.query(Word).filter(Word.language == language).all()]
+def get_all_words(session: Session, language: Language) -> list[str]:
+    """Returns all words in a given Language."""
+
+    return [word.word for word in session.query(Word).filter(Word.language == language).all()]
 
 
-def get_word_history(word_id: int, date: date = date.today()) -> WordHistory:
-    with open_session() as session:
-        return session.query(WordHistory).filter(WordHistory.word_id == word_id, WordHistory.date == date).first()
+def get_word_history(session: Session, word_id: int, date: date = date.today()) -> WordHistory:  # noqa: B008
+    """Returns the word history for a given word and date, date defaults to today."""
+
+    return session.query(WordHistory).filter(WordHistory.word_id == word_id, WordHistory.date == date).first()
 
 
-def reset_words() -> None:
+def reset_words(session: Session) -> None:
+    """Resets the words. If no word for today exists, a new one is generated."""
+
     for language in Language:
         try:
-            get_word_today(language)
+            get_word_today(session, language)
         except ValueError:
-            generate_word_today(language)
+            generate_word_today(session, language)
