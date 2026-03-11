837f9598d068ad9763f1cdec9a84493aa4624bcf
Moritz Pahlke <moritz.pahlke@uni-konstanz.de>
2026-03-11 13:31:24 +0100

    cleanup guess_history

--- function/database/guess_history.py
+++ function/database/guess_history.py
@@ -1,14 +1,12 @@
 from sqlalchemy.orm import Session
 
-from database.database import open_session
 from database.models import GuessHistory
 
 
-def add_new_user_guess(user_id: int, word_history_id: int, guess: str) -> None:
+def add_new_user_guess(session: Session, user_id: int, word_history_id: int, guess: str) -> None:
     """Adds an user guess to the database"""
 
-    with open_session() as session:
-        session.add(GuessHistory(user_id=user_id, word_history_id=word_history_id, guess=guess))
+    session.add(GuessHistory(user_id=user_id, word_history_id=word_history_id, guess=guess))
 
 
 def get_user_guess_history(session: Session, user_id: int, word_history_id: int) -> list[GuessHistory]:
