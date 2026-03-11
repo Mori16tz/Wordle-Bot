340aea4f9a754ccabe94346a33392f548a4152cd
haeffema <haeffema@gmail.com>
2026-03-11 14:35:11 +0100

    fix 4

--- function/database/guess_history.py
+++ function/database/guess_history.py
@@ -1,19 +1,19 @@
 from sqlalchemy.orm import Session
 
-from database.models import GuessHistory
+from database.models import GuessHistory, User, WordHistory
 
 
-def add_new_user_guess(session: Session, user_id: int, word_history_id: int, guess: str) -> None:
+def add_new_user_guess(session: Session, user: User, word_history: WordHistory, guess: str) -> None:
     """Adds an user guess to the database."""
 
-    session.add(GuessHistory(user_id=user_id, word_history_id=word_history_id, guess=guess))
+    session.add(GuessHistory(user_id=user.id, word_history_id=word_history.id, guess=guess))
 
 
-def get_user_guess_history(session: Session, user_id: int, word_history_id: int) -> list[GuessHistory]:
+def get_user_guess_history(session: Session, user: User, word_history: WordHistory) -> list[GuessHistory]:
     """Returns the user guess history for a given word."""
 
     return (
         session.query(GuessHistory)
-        .filter(GuessHistory.user_id == user_id, GuessHistory.word_history_id == word_history_id)
+        .filter(GuessHistory.user_id == user.id, GuessHistory.word_history_id == word_history.id)
         .all()
     )
