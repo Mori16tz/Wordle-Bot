ec88ebc07d4f7d2bf6a5372046911a8cbe519c24
Moritz <moritz.pahlke@web.de>
2026-07-30 01:15:58 +0200

    update database security

--- function/common/consts.py
+++ function/common/consts.py
@@ -5,3 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("TOKEN", "no token set")
 OWNER_ID = int(os.getenv("OWNER", "-1"))
+DB_PASSWORD = os.getenv("DB_PASSWORD", -1)
--- function/database/database.py
+++ function/database/database.py
@@ -1,13 +1,18 @@
 from contextlib import contextmanager
 from typing import Iterator
 
+from common.consts import DB_PASSWORD
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import Session, sessionmaker
 
 from database.models import Base
 
 engine = create_engine(
-    "mysql+pymysql://root:@localhost:3306/wordle", pool_pre_ping=True, pool_recycle=3600, pool_size=5, max_overflow=10
+    f"mysql+pymysql://root:{DB_PASSWORD}@localhost:3306/wordle",
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    pool_size=5,
+    max_overflow=10,
 )
 
 inspector = inspect(engine)
